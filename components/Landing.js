93df21738acadfea36bee289b83771779d08ff1f
wulf <prydax997@gmail.com>
2023-03-13 02:40:35 +0000

    landing page animations

--- components/Landing.js
+++ components/Landing.js
@@ -1,12 +1,23 @@
 import Experience from "./canvas/Experience"
-
 import styles from '../styles/landing.module.css'
+import gsap from 'gsap'
+import { useLayoutEffect, useRef } from "react"
 
 function Landing(){
+
+    const topline = useRef()
+    const bottomline = useRef()
+
+    useLayoutEffect(() => {
+        let context = gsap.context(() => {
+            gsap.to([topline.current, bottomline.current], {width: '95%', ease: 'power2.inOut', duration: 2, delay: 1, stagger: 0.2})
+        })
+    })
+
     return (
         <div className={styles.landingContainer}>
             <div className={styles.ruleContainer}>
-                <div className={styles.lineRule} />
+                <div className={styles.lineRule} ref={topline}/>
             </div>
             
             <div className={styles.canvasContainer}>
@@ -14,7 +25,7 @@ function Landing(){
             </div>
             
             <div className={styles.ruleContainer}>
-                <div className={styles.lineRule} />
+                <div className={styles.lineRule} ref={bottomline} />
             </div>
         </div>
     )
--- components/canvas/HomeText.js
+++ components/canvas/HomeText.js
@@ -1,22 +1,46 @@
-import { Text } from "@react-three/drei"
-import { useThree } from "@react-three/fiber"
-import { useEffect } from "react"
+import { Text, shaderMaterial } from "@react-three/drei"
+import { useFrame, useThree } from "@react-three/fiber"
+import { use, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react"
+import * as THREE from 'three'
+import gsap from 'gsap'
+
+
 
 function HomeText(){
+    
+    const [hovered, setHovered] = useState(null)
+    const color = useMemo(() => {
+        return new THREE.Color()
+    }, [hovered])
     const {size} = useThree()
+    const text = useRef()
 
-    useEffect(() => {
-        
-        console.log(size)
+    const over = (e) => (e.stopPropagation(), setHovered(true))
+    const out = () => {setHovered(false)}
+
+    useFrame((state, delta) => {
+        text.current.material.color.lerp(color.set(hovered ? 'salmon' : 'black'), 0.2)
+    })
+
+    useLayoutEffect(() => {
+        let context = gsap.context(() => {
+            gsap.to(text.current, { fillOpacity: 1, ease: 'power2.inOut', duration: 2})
+        })
     })
 
     return (
             <Text 
+                ref={text}
                 font='/fonts/cairo-v22-latin-regular.woff'
+                lineHeight={0.7}
                 fontSize={2 * size.width/size.height}
                 color='black'
+                fillOpacity={0}
+                letterSpacing={0.2}
+                onPointerOver={over}
+                onPointerOut={out}
                 >
-                    H A R I S
+                    HARIS
             </Text>
     )
 }
